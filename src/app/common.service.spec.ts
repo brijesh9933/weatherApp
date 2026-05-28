605117ed575381d36c346e1d1959fae6ddf18377
Brijesh Sharma <brijesharma69@gmail.com>
2026-05-28 14:29:22 +0530

    test cases added, sonar integrated

--- src/app/common.service.spec.ts
+++ src/app/common.service.spec.ts
@@ -1,14 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { CommonService } from './common.service';
+
 
 describe('CommonService', () => {
   let service: CommonService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [CommonService]
+    });
+
     service = TestBed.inject(CommonService);
   });
+
+
+
 
   it('should be created', () => {
     expect(service).toBeTruthy();
